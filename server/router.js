204bd61710077e9d073219441570fac94c517ea0
dr3nan <dolgarrenan@gmail.com>
2023-03-10 13:15:29 +0100

    chore: initial router file configuration, refs #101

---
+++ server/router.js
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const router = express.Router();
+
+// we define the endpoint for the GET request
+
+// we define the endpoint for the POST request
+
+// we define the endpoint for the PUT request
+
+// we define the endpoint for the DELETE request
+
+export default router;
